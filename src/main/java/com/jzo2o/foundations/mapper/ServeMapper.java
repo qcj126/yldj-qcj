60ca3b4eff28bea950c974be5e1eeabf6a8a4159
mrt <your_email@example.com>
2024-09-30 22:39:55 +0800

    根据授课安排修复代码

--- src/main/java/com/jzo2o/foundations/mapper/ServeMapper.java
+++ src/main/java/com/jzo2o/foundations/mapper/ServeMapper.java
@@ -1,15 +1,7 @@
 package com.jzo2o.foundations.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jzo2o.api.foundations.dto.response.ServeAggregationResDTO;
 import com.jzo2o.foundations.model.domain.Serve;
-import com.jzo2o.foundations.model.dto.response.ServeAggregationSimpleResDTO;
-import com.jzo2o.foundations.model.dto.response.ServeAggregationTypeSimpleResDTO;
-import com.jzo2o.foundations.model.dto.response.ServeCategoryResDTO;
-import com.jzo2o.foundations.model.dto.response.ServeResDTO;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 /**
  * <p>
@@ -20,7 +12,4 @@ import java.util.List;
  * @since 2023-07-03
  */
 public interface ServeMapper extends BaseMapper<Serve> {
-
-    //根据区域查询服务列表
-    List<ServeResDTO> queryServeListByRegionId(@Param("regionId") Long regionId);
 }
